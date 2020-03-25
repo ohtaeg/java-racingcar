21f93757ee98c665d31dcf7f9f55c3d6c11de8c6
ohtaeg <otk1090@naver.com>
2020-03-26 03:21:23 +0900

    refactor (Winners) : equals, hashcode 재정의 및 거리 비교 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import racingcar.policy.MovingPolicy;
 
+import java.util.Objects;
+
 public final class Car implements Comparable<Car> {
     private final CarName name;
     private Distance distance;
@@ -29,8 +31,26 @@ public final class Car implements Comparable<Car> {
         return distance;
     }
 
+    public boolean match(final Car other) {
+        return distance.equals(other.distance);
+    }
+
     @Override
     public int compareTo(final Car car) {
         return distance.compare(car.distance);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Car car = (Car) o;
+        return Objects.equals(name, car.name) &&
+                Objects.equals(distance, car.distance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
     }
 }
