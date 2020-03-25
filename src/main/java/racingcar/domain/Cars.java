d576f6b1ca2ee4171d55607b089435b75b016e13
ohtaeg <otk1090@naver.com>
2020-03-26 03:23:17 +0900

    refactor (Cars) : 우승자 수상 메서드 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,8 +4,12 @@ import racingcar.policy.MovingPolicy;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Cars {
+    private static final String WIN_MESSAGE = "가 최종 우승했습니다.";
+    private static final String WINNER_JOIN_EXPRESSION = ", ";
+
     private final List<Car> cars;
 
     public Cars(final List<Car> cars) {
@@ -20,5 +24,14 @@ public final class Cars {
 
     public List<Car> toList() {
         return Collections.unmodifiableList(cars);
+    }
+
+    public String award() {
+        String award = cars.stream()
+                           .map(Car::getName)
+                           .map(CarName::toString)
+                           .collect(Collectors.joining(WINNER_JOIN_EXPRESSION))
+        ;
+        return award + WIN_MESSAGE;
     }
 }
