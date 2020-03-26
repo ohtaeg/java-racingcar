08fb38451670ded1b3f3d692c12cfc874de2ffd8
ohtaeg <otk1090@naver.com>
2020-03-26 15:16:55 +0900

    refactor (Outputview) : 우승자 출력 로직 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,15 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
+import racingcar.domain.CarName;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public static final String GAME_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_JOIN_EXPRESSION = ", ";
+    private static final String WIN_MESSAGE = "가 최종 우승했습니다.";
 
     public OutputView() {}
 
@@ -27,5 +31,13 @@ public class OutputView {
 
     public void nextLine() {
         System.out.println();
+    }
+
+    public void award(final List<Car> winners) {
+        String award = winners.stream()
+                              .map(Car::getName)
+                              .map(CarName::toString)
+                              .collect(Collectors.joining(WINNER_JOIN_EXPRESSION));
+        print(award + WIN_MESSAGE);
     }
 }
