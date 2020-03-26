8fd001beb6fcfc45793bcffa02d8b0e28243a586
ohtaeg <otk1090@naver.com>
2020-03-26 15:13:12 +0900

    test (Cars) : 우승자 구하는 로직 테스트

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -3,12 +3,11 @@ package racingcar.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
-import racingcar.domain.CarName;
-import racingcar.domain.Cars;
+import racingcar.domain.*;
 import racingcar.policy.fake.SuccessMovingPolicy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,5 +34,21 @@ class CarsTest {
         for (Car car : cars.toList()) {
             assertThat(car.currentDistance().toInt()).isEqualTo(1);
         }
+    }
+
+    @DisplayName("우승자를 구할 수 있다.")
+    @Test
+    void findWinners() {
+        // given
+        Car pobi = new Car(new CarName("pobi"), new Distance(3));
+        Car ohtaeg = new Car(new CarName("ohtaeg"), new Distance(2));
+        Car nana = new Car(new CarName("nana"), new Distance(3));
+        Cars cars = new Cars(Arrays.asList(pobi, ohtaeg, nana));
+
+        // when
+        List<Car> winners = cars.findWinners();
+
+        assertThat(winners).hasSize(2);
+        assertThat(winners).contains(pobi, nana);
     }
 }
